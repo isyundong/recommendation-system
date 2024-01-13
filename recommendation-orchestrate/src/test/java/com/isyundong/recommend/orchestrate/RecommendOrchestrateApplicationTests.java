ac43620359c381be0a7e9cfc7437cd5fe736824a
isyundong <yundong.zhang@aliyun.com>
2024-01-13 22:22:17 +0800

    [RECOMMENDATION-SYSTEM] first time submitting code, init project

---
+++ recommendation-orchestrate/src/test/java/com/isyundong/recommend/orchestrate/RecommendOrchestrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isyundong.recommend.orchestrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendOrchestrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
